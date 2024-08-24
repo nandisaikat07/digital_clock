77931fd5c1a531c96d5a121004b1d776a4f17452
Saikat Nandi <107092287+nandisaikat07@users.noreply.github.com>
2024-08-24 17:41:16 +0530

    Add files via upload

---
+++ project1 (2).js
@@ -0,0 +1,47 @@
+var clockInterval;
+
+        function updateClock(){
+            var now = new Date();
+            var dname = now.getDay();
+            var mo = now.getMonth();
+            var dnum = now.getDate();
+            var yr = now.getFullYear();
+            var hou = now.getHours();
+            var min = now.getMinutes();
+            var sec = now.getSeconds();
+            var pe = "AM";
+
+          
+            if (hou > 12) {
+                hou = hou - 12;
+                pe = "PM";
+            }
+
+            Number.prototype.pad = function(digits) {
+                for (var n = this.toString(); n.length < digits; n = "0" + n);
+                return n;
+            }
+
+            var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+            var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
+            var values = [week[dname], months[mo], dnum.pad(2), yr, hou.pad(2), min.pad(2), sec.pad(2), pe];
+            for (var i = 0; i < ids.length; i++) {
+                document.getElementById(ids[i]).firstChild.nodeValue = values[i];
+            }
+        }
+
+        function startClock() {
+            clockInterval = setInterval(updateClock, 1000);
+            updateClock();
+        }
+
+        function stopClock() {
+            clearInterval(clockInterval);
+        }
+
+        document.getElementById("startBtn").addEventListener("click", startClock);
+        function changebackgroundcolor(){}
+        document.getElementById("start").style.backgroundColor="yellow";
+         document.getElementById("stopBtn").addEventListener("click", stopClock);
+document.getElementById("start").style.backgroundColor="yellow";
